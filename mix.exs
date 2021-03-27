eeba4da43a8aeb6f7205c227036574991232d3d3
Jakub Gonet <jakub.gonet@swmansion.com>
2021-03-27 16:16:19 +0100

    Add Credo

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule PlanPicker.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:ecto_psql_extras, "~> 0.2"}
+      {:ecto_psql_extras, "~> 0.2"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
