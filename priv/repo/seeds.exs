fe023b51495d87814ce564d5c15ccd45583de03f
Karol Wilk <Wilczur.new@gmail.com>
2021-05-10 00:33:31 +0200

    added admin and moderator base users to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,7 +10,25 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-admin = %PlanPicker.Accounts.User{email: "admin@test.com", hashed_password: Bcrypt.hash_pwd_salt("admin"), name: "admin", last_name: "admin", index_no: "000000"}
-|> PlanPicker.Repo.insert!(on_conflict: :nothing)
+import Ecto.Query, only: [from: 2]
+
+PlanPicker.Repo.delete_all(from role in PlanPicker.Role, select: role)
+
+admin = case PlanPicker.Repo.get_by(PlanPicker.Accounts.User, email: "admin@test.com") do
+  nil ->
+    %PlanPicker.Accounts.User{email: "admin@test.com", hashed_password: Bcrypt.hash_pwd_salt("admin"), name: "admin", last_name: "admin", index_no: "000000"}
+    |> PlanPicker.Repo.insert!()
+  r -> r
+end
+
 PlanPicker.Role.assign_role(admin, :moderator)
 PlanPicker.Role.assign_role(admin, :admin)
+
+moderator = case PlanPicker.Repo.get_by(PlanPicker.Accounts.User, email: "moderator@test.com") do
+  nil ->
+    %PlanPicker.Accounts.User{email: "moderator@test.com", hashed_password: Bcrypt.hash_pwd_salt("moderator"), name: "moderator", last_name: "moderator", index_no: "000001"}
+    |> PlanPicker.Repo.insert!()
+  r -> r
+end
+
+PlanPicker.Role.assign_role(moderator, :moderator)
