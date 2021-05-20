a562ef0cc248d07855f1fcfd96ea64481f2e04e6
Jakub Gonet <jakub.gonet@swmansion.com>
2021-05-20 18:19:42 +0200

    Remove dead code in seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,25 +10,25 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-import Ecto.Query, only: [from: 2]
-
-PlanPicker.Repo.delete_all(from role in PlanPicker.Role, select: role)
-
-admin = case PlanPicker.Repo.get_by(PlanPicker.Accounts.User, email: "admin@test.com") do
-  nil ->
-    %PlanPicker.Accounts.User{email: "admin@test.com", hashed_password: Bcrypt.hash_pwd_salt("admin"), name: "admin", last_name: "admin", index_no: "000000"}
-    |> PlanPicker.Repo.insert!()
-  r -> r
-end
+admin =
+  PlanPicker.Repo.insert!(%PlanPicker.Accounts.User{
+    email: "admin@test.com",
+    hashed_password: Bcrypt.hash_pwd_salt("admin"),
+    name: "admin",
+    last_name: "admin",
+    index_no: "000000"
+  })
 
 PlanPicker.Role.assign_role(admin, :moderator)
 PlanPicker.Role.assign_role(admin, :admin)
 
-moderator = case PlanPicker.Repo.get_by(PlanPicker.Accounts.User, email: "moderator@test.com") do
-  nil ->
-    %PlanPicker.Accounts.User{email: "moderator@test.com", hashed_password: Bcrypt.hash_pwd_salt("moderator"), name: "moderator", last_name: "moderator", index_no: "000001"}
-    |> PlanPicker.Repo.insert!()
-  r -> r
-end
+moderator =
+  PlanPicker.Repo.insert!(%PlanPicker.Accounts.User{
+    email: "moderator@test.com",
+    hashed_password: Bcrypt.hash_pwd_salt("moderator"),
+    name: "moderator",
+    last_name: "moderator",
+    index_no: "000001"
+  })
 
 PlanPicker.Role.assign_role(moderator, :moderator)
