a3c532a873a697eab397247d6a4a120be86fec1b
Jakub Gonet <jakub.gonet@swmansion.com>
2021-05-21 13:52:57 +0200

    Bump ecto_sql to 3.6

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule PlanPicker.MixProject do
       {:bcrypt_elixir, "~> 2.0"},
       {:phoenix, "~> 1.5.8"},
       {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
+      {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
