986351b9a058e32a05123206db5a030aa43a7509
Jakub Gonet <jakub.gonet@swmansion.com>
2021-05-30 18:32:44 +0200

    Add Timestamp.Range.from_time/3

--- lib/plan_picker/term.ex
+++ lib/plan_picker/term.ex
@@ -6,7 +6,7 @@ defmodule PlanPicker.Term do
     field :interval, Timestamp.Range
     field :interval_time_start, :time, virtual: true
     field :interval_time_end, :time, virtual: true
-    field :interval_day_offset, :integer, virtual: true
+    field :interval_weekday, Ecto.Enum, virtual: true, values: Timestamp.Day.allowed_days()
 
     field :location, :string
     field :week_type, :string
@@ -30,7 +30,7 @@ defmodule PlanPicker.Term do
       :week_type,
       :interval_time_start,
       :interval_time_end,
-      :interval_day_offset
+      :interval_weekday
     ])
     |> set_interval()
     |> validate_required([:interval, :location, :week_type])
@@ -39,22 +39,10 @@ defmodule PlanPicker.Term do
   defp set_interval(changeset) do
     from = get_field(changeset, :interval_time_start)
     to = get_field(changeset, :interval_time_end)
-    offset = get_field(changeset, :interval_day_offset)
+    weekday = get_field(changeset, :interval_weekday)
 
-    if from && to && offset do
-      interval_start =
-        DateTime.new!(
-          Date.new!(1996, 1, 1 + offset),
-          from
-        )
-
-      interval_end =
-        DateTime.new!(
-          Date.new!(1996, 1, 1 + offset),
-          to
-        )
-
-      put_change(changeset, :interval, Timestamp.Range.new(interval_start, interval_end))
+    if from && to && weekday do
+      put_change(changeset, :interval, Timestamp.Range.from_time(from, to, weekday))
     else
       changeset
     end
---
+++ lib/plan_picker/timestamp/days.ex
@@ -0,0 +1,9 @@
+defmodule Timestamp.Day do
+  @allowed_days_keys [:monday, :tuesday, :wednesday, :thursday, :friday]
+  @allowed_days @allowed_days_keys
+                |> Enum.with_index()
+                |> Map.new()
+
+  def allowed_days, do: @allowed_days_keys
+  def get_offset(day) when day in @allowed_days_keys, do: @allowed_days[day]
+end
