61a98c7784fed0c380b79f3ab049c32280f09622
Jakub Gonet <jakub.gonet@swmansion.com>
2021-05-30 20:30:19 +0200

    Simplify Term interval handling

--- lib/plan_picker/term.ex
+++ lib/plan_picker/term.ex
@@ -4,10 +4,6 @@ defmodule PlanPicker.Term do
 
   schema "terms" do
     field :interval, Timestamp.Range
-    field :interval_time_start, :time, virtual: true
-    field :interval_time_end, :time, virtual: true
-    field :interval_weekday, Ecto.Enum, virtual: true, values: Timestamp.Day.allowed_days()
-
     field :location, :string
     field :week_type, :string
     belongs_to :class, PlanPicker.Class
@@ -28,21 +24,16 @@ defmodule PlanPicker.Term do
     |> cast(attrs, [
       :location,
       :week_type,
-      :interval_time_start,
-      :interval_time_end,
-      :interval_weekday
+      :interval,
     ])
     |> set_interval()
     |> validate_required([:interval, :location, :week_type])
   end
 
   defp set_interval(changeset) do
-    from = get_field(changeset, :interval_time_start)
-    to = get_field(changeset, :interval_time_end)
-    weekday = get_field(changeset, :interval_weekday)
-
-    if from && to && weekday do
-      put_change(changeset, :interval, Timestamp.Range.from_time(from, to, weekday))
+    {start: start_t, end: end_t, weekday: weekday} = get_field(changeset, :interval)
+    if start_t && end_t && weekday do
+      put_change(changeset, :interval, Timestamp.Range.from_time(start_t, end_t, weekday))
     else
       changeset
     end
