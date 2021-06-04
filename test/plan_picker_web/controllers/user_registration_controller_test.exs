02cbd758e75ee25b77c409e87ffcff4f5de47a9c
Jakub <jakub.gonet0@gmail.com>
2021-06-04 18:44:06 +0200

    Add Bulma css framework (#34)

--- test/plan_picker_web/controllers/user_registration_controller_test.exs
+++ test/plan_picker_web/controllers/user_registration_controller_test.exs
@@ -8,8 +8,8 @@ defmodule PlanPickerWeb.UserRegistrationControllerTest do
       conn = get(conn, Routes.user_registration_path(conn, :new))
       response = html_response(conn, 200)
       assert response =~ "<h1>Register</h1>"
+      assert response =~ "Register</button>"
       assert response =~ "Log in</a>"
-      assert response =~ "Register</a>"
     end
 
     test "redirects if already logged in", %{conn: conn} do
