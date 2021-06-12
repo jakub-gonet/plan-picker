af6ee093213f87dac16956ec10937dfff077b6b7
Jakub <jakub.gonet0@gmail.com>
2021-06-12 12:05:11 +0200

    Update seeds (#36)

--- priv/repo/migrations/20210417133131_add_initial_schema.exs
+++ priv/repo/migrations/20210417133131_add_initial_schema.exs
@@ -157,12 +157,13 @@ defmodule PlanPicker.Repo.Migrations.AddInitialSchema do
       timestamps()
     end
 
-    create table(:enrollments_users) do
+    create table(:enrollments_users, primary_key: false) do
       add :user_id, references(:users, on_delete: :delete_all)
       add :enrollment_id, references(:enrollments, on_delete: :delete_all)
-
-      timestamps()
     end
+
+    create(index(:enrollments_users, [:user_id]))
+    create(index(:enrollments_users, [:enrollment_id]))
 
     create unique_index(:enrollments_users, [:user_id, :enrollment_id],
              name: "enrollments_users_unique_fk"
